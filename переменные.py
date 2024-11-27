249038db2209adbcc6a45fda64771afae3191c2e
KOPEHb-Pro <konstantin.korinevskiy@gmail.com>
2024-11-28 01:37:58 +0800

    Update and rename переменные.py to Module 1/module_1.1.py

--- переменные.py
+++
@@ -1,5 +0,0 @@
-Count_HW = 12
-Count_of_spent_hours = 1.5
-Course = 'Python'
-Time_for_one_task = Count_of_spent_hours/Count_HW
-print('Курс:', Course, ',', 'всего задач:', Count_HW, ',', 'затрачено часов:', Count_of_spent_hours, ',', 'среднее время выполнения:', Time_for_one_task, 'часа.' )
